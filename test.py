2cbe039688568da71c1560bba53de0a29e63d5f5
Robert Hönig <robhoenig@gmail.com>
2019-08-05 19:00:42 +0100

    test.py: Use tf.test.TestCase instead of unittest.TestCase.
    Also fix TestLazyWaveletLayer.test_forward().

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
-import unittest
-
 # See https://github.com/tensorflow/tensorflow/issues/31249
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
@@ -9,28 +7,27 @@ import tensorflow_probability as tfp
 
 from main import LazyWavelet, Lifting
 
-class TestLazyWaveletLayer(unittest.TestCase):
+class TestLazyWaveletLayer(tf.test.TestCase):
     def test_forward(self):
-        def test_lazy_wavelet_layer(x, y_expected):
-            x = tf.constant([1,2,3,4,5,6,7,8], dtype='float32')
-            y_expected = tf.constant([[1,3,5,7], [2,4,6,8]], dtype='float32')
-            lazy_layer = LazyWavelet()
-            y_result = lazy_layer._forward(x)
-            self.assertTrue(tf.reduce_all(tf.math.equal(y_expected, y_result)))
+        x = tf.constant([1,2,3,4,5,6,7,8], dtype='float32')
+        y_expected = tf.constant([[1,3,5,7], [2,4,6,8]], dtype='float32')
+        lazy_layer = LazyWavelet()
+        y_result = lazy_layer._forward(x)
+        self.assertAllEqual(y_expected, y_result)
 
     def test_inverse(self):
         lazy_layer = LazyWavelet()
         x = tf.constant([1,2,3,4,5,6,7,8], dtype='float32')
         y = lazy_layer._forward(x)
         y_inv = lazy_layer._inverse(y)
-        self.assertLess(tf.norm(x-y_inv), 1e-06)
+        self.assertAllClose(x, y_inv)
 
-class TestLiftingLayer(unittest.TestCase):
+class TestLiftingLayer(tf.test.TestCase):
     def test_forward(self):
         def test_x_transforms_to_y_expected(x, P_coeff, U_coeff, y_expected):
             lifting = Lifting(P_coeff=P_coeff, U_coeff=U_coeff)
             y_result = lifting.forward(x)
-            self.assertTrue(tf.reduce_all(tf.math.equal(y_expected, y_result)))
+            self.assertAllEqual(y_expected, y_result)
 
         # Haar wavelet, verified with MATLAB:
         # y_expected = dwt(x, [1/2, 1/2], [1, -1]))
@@ -53,22 +50,22 @@ class TestLiftingLayer(unittest.TestCase):
         x = tf.constant([[1,3,5,7], [2,4,6,8]], dtype='float32')
         y = lifting._forward(x)
         y_inv = lifting._inverse(y)
-        self.assertLess(tf.norm(x-y_inv), 1e-06)
+        self.assertAllClose(x, y_inv)
 
-class TestChaining(unittest.TestCase):
+class TestChaining(tf.test.TestCase):
     def test_chain_of_lifting_and_lazy_wavelet_forward(self):
         x = tf.constant([1,2,3,4,5,6,7,8], dtype='float32')
         y_expected = tf.constant([[1., 4., 6., 7.], [-2., 2., 2., 2.]])
         chain = tfp.bijectors.Chain([Lifting(P_coeff=tf.constant([0, .5, .5]),U_coeff=tf.constant([.25, .25, 0])), LazyWavelet()])
         y_result = chain.forward(x)
-        self.assertTrue(tf.reduce_all(tf.math.equal(y_expected, y_result)))
+        self.assertAllEqual(y_expected, y_result)
 
     def test_chain_of_lifting_and_lazy_wavelet_inverse(self):
         x = tf.constant([1,2,3,4,5,6,7,8], dtype='float32')
         chain = tfp.bijectors.Chain([Lifting(P_coeff=tf.constant([0, .5, .5]),U_coeff=tf.constant([.25, .25, 0])), LazyWavelet()])
         y = chain.forward(x)
         y_inv = chain._inverse(y)
-        self.assertLess(tf.norm(x-y_inv), 1e-06)
+        self.assertAllClose(x, y_inv)
 
 if __name__ == '__main__':
     unittest.main()
